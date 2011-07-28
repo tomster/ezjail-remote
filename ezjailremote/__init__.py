d7d7b812b8ce96716fdd31a317635f333e01e73f
Tom Lazar <tom@tomster.org>
2011-07-29 00:22:09 +0200

    instead of evil monkey patch, simply inject the -f command to point to our fabfile. this has the additional benefit of allowing to override the location of the fabfile on the command line.

--- ezjailremote/__init__.py
+++ ezjailremote/__init__.py
@@ -1,11 +1,11 @@
+import sys
 from os import path
 from fabric import main
 
 here = path.abspath(path.dirname(__file__))
 
-def this_fabfile():
-    return path.join(here, 'fabfile.py')
-
 def commandline():
-    main.find_fabfile = this_fabfile
+    # default to our own fabfile
+    if '-f' not in sys.argv:
+        sys.argv.extend(['-f', path.join(here, 'fabfile.py')])
     main.main()
